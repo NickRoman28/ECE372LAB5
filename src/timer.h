6989b2551deb6fa5eaeebcb7eeba313b0946a2fc
AidanDeo27 <aidandeo@arizona.edu>
2026-04-13 12:07:22 -0700

    Add timer.h header file with timer functionslast timer from lab 4

---
+++ src/timer.h
@@ -0,0 +1,23 @@
+// Author:         
+// Net ID:         
+// Date:           
+// Assignment:     Lab 3
+//----------------------------------------------------------------------//
+
+#ifndef TIMER_H
+#define TIMER_H
+
+#include <avr/io.h>
+
+void initTimer1();
+void delaySeconds(unsigned int delay);
+
+void startTimer1();
+void stopTimer1();
+
+void initTimer0();
+void delayMs(unsigned int delay); 
+
+extern volatile unsigned char second;
+
+#endif
