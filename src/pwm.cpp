72502390c40189446efeef5931fb9dd9101c3045
nmonroe04 <167247190+nmonroe04@users.noreply.github.com>
2026-04-13 12:27:24 -0700

    Update pwm.cpp

--- src/pwm.cpp
+++ src/pwm.cpp
@@ -7,33 +7,43 @@
 void initPWM3(void)
 {
     // we have to edit the OCRnA registers
-     // digital pin 5
+     // digital pin 5 (GOOD)
     DDRE |= (1 << DDE3);
 
     // Clear timer registers first using timer3
-    TCCR3A = 0x00;
-    TCCR3B = 0x00;
+    TCCR4A = 0x00;
+    TCCR4B = 0x00;
 
+
+  //table 17-2, we need fast PWM, mode 15 for variable frequency (we used mode 14 for lab 4 i think)
+    TCCR4A |= (1<<WGM40) | (1<<WGM41);
+    TCCR4B |= (1<<WGM42) | (1<<WGM43); 
     // table 17-4, Fast PWM non-inverting mode
-    // what this does is sets the 0 of the 10 bit number from adc to be off and 1023 to be on 100% or 5V
-    TCCR3A |= (1 << COM3A1);
-    TCCR3A &= ~(1 << COM3A0);
-
-    // fast PWM 10 bit mode for fixed frequency table 17-2 14
-    TCCR3A |= (1 << WGM30) | (1 << WGM31);
-    TCCR3B |= (1 << WGM32);
-    TCCR3B &= ~(1 << WGM33);
+    TCCR4A &= ~(1<<COM4C0); 
+    TCCR4A |= (1<<COM4C1);
+    //^^^ sets the 0 of the 10 bit number from adc to be off and 1023 to be on 100% or 5V
 
     // set the prescaler equal to 1
     // this way we can use a top value of 1023 to get 15,625Hz using lecture 13 DC motor formula
     TCCR3B |= (1 << CS30);
     TCCR3B &= ~((1 << CS31) | (1 << CS32));
 
-    // starts at 0% duty cycle 
-    OCR3A = 0;
+    //table 17-6, set prescaler to 1
+    TCCR4B |= (1<<CS40);
+    DDRH |= (1<<DDH5); //Set Pin 8 on board to output, which is OC4C, PORTH5, PWM}
+
+    // starts at 0% duty cycle (NEED TO FIX POSSIBLY, )
+    OCR4A = 0;
 }
 
-void changeDutyCycle(unsigned int adcVal)
+// WILL HAVE TO CONFIRM THAT THESE NUMBERS ARE RIGHT (WILL DELETE THIS MESSAGE WHEN COMPELTE)
+void incFrequency(unsigned int frequency) {
+OCR4A = 16000000 / frequency; //update the TOP based on the new frequency
+OCR4C=0.5*OCR4A;// update the OCR4C to make sure the Duty cycle stay the same
+}
+
+//below is what we used in lab 4 for fixed freq
+/* void changeDutyCycle(unsigned int adcVal)
 {
     // set the value to a 10 bit range
     if (adcVal > 1023)
@@ -43,4 +53,4 @@ void changeDutyCycle(unsigned int adcVal)
 
     // takes the adc value and converts it into PWM duty cycle
     OCR3A = adcVal;
-}
+} */
