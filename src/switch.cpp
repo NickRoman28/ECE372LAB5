f12284f4a799492244c00f36f7f232ca39f96da6
AidanDeo27 <aidandeo@arizona.edu>
2026-04-20 12:04:22 -0700

    switch done

---
+++ src/switch.cpp
@@ -0,0 +1,23 @@
+// LAB4 ECE372
+// NICK ROMAN, AIDAN DEOGRACIAS, NOAH MONROE, ANDREW GHARTEY
+// COMPLETED 4/6/26
+// Description: This file implements the initialization of an external
+// switch.
+//----------------------------------------------------------------------//
+
+#include "switch.h"
+#include <avr/io.h>
+/*
+ * Initializes pull-up resistor on PD0 and sets it into input mode
+ */
+
+void initSwitchPD0(){
+    //initialize as input using and with a 0
+    DDRD &= ~(1<<PD0);
+    //initialize the pullup resistor
+    PORTD |= (1 << PD0);
+    //initialize/enable external interrupts for PD0 (INT0)
+    EICRA &= ~(1 << ISC01);
+    EICRA |= (1 << ISC00);
+    EIMSK |= (1<<INT0);
+}
