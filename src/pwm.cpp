ab093cde889310ef4ca8a0582cf9e644f8f715a8
agharteyaz <167935447+agharteyaz@users.noreply.github.com>
2026-04-27 12:45:54 -0700

    update pwm

--- src/pwm.cpp
+++ src/pwm.cpp
@@ -1,4 +1,4 @@
-// LAB4 ECE372
+// LAB5 ECE372
 // NICK ROMAN, AIDAN DEOGRACIAS, NOAH MONROE, ANDREW GHARTEY
 // COMPLETED 4/6/26
 #include <avr/io.h>
@@ -6,46 +6,41 @@
 
 void initPWM(void)
 {
-    // we have to edit the OCRnA registers
-     // digital pin 6 to output which uses timer 4 (GOOD)
-    DDRH |= (1 << DDH3);
+    // OC4C is on PH5 (pin 8) - corrected from DDH3
+    DDRH |= (1 << DDH5);
 
-    //CHECK 
-
-    
     // Clear timer 4 registers 
     TCCR4A = 0x00;
     TCCR4B = 0x00;
 
+    // Fast PWM mode 15 (WGM43:40 = 1111), TOP = OCR4A
+    TCCR4A |= (1 << WGM40) | (1 << WGM41);
+    TCCR4B |= (1 << WGM42) | (1 << WGM43); 
 
-  //table 17-2, we need fast PWM, mode 15 for variable frequency (we used mode 14 for lab 4 i think)
-    TCCR4A |= (1<<WGM40) | (1<<WGM41);
-    TCCR4B |= (1<<WGM42) | (1<<WGM43); 
-    // table 17-4, Fast PWM non-inverting mode
-    TCCR4A &= ~(1<<COM4C0); 
-    TCCR4A |= (1<<COM4C1);
-    //^^^ sets the 0 of the 10 bit number from adc to be off and 1023 to be on 100% or 5V
+    // Non-inverting mode on channel C (OC4C)
+    TCCR4A &= ~(1 << COM4C0); 
+    TCCR4A |= (1 << COM4C1);
 
-    // set the prescaler equal to 1
-    // this way we can use a top value of 1023 to get 15,625Hz using lecture 13 DC motor formula
-    //table 17-6, set prescaler to 1
-    TCCR4B |= (1<<CS40);
-   
+    // Prescaler = 1
+    TCCR4B |= (1 << CS40);
 
-    // starts at 0% duty cycle (NEED TO FIX POSSIBLY, )
+    // Start silent
     OCR4A = 0;
+    OCR4C = 0;
 }
 
-// WILL HAVE TO CONFIRM THAT THESE NUMBERS ARE RIGHT (WILL DELETE THIS MESSAGE WHEN COMPELTE)
 void incFrequency(unsigned int frequency) {
-     if (frequency == 0) { OCR4A = 0; OCR4C = 0; return; }
-    OCR4A = 16000000 / frequency; //update the TOP based on the new frequency
-    OCR4C=0.5*OCR4A;// update the OCR4C to make sure the Duty cycle stay the same
+    if (frequency == 0) { 
+        OCR4A = 0; 
+        OCR4C = 0; 
+        return; 
+    }
+    OCR4A = 16000000UL / frequency;
+    OCR4C = OCR4A / 2;  // 50% duty cycle
 }
- void setFrequency(unsigned int frequency)
-{
-    unsigned long top;
 
+void setFrequency(unsigned int frequency)
+{
     if (frequency == 0)
     {
         OCR4A = 0;
@@ -53,11 +48,9 @@ void incFrequency(unsigned int frequency) {
         return;
     }
 
-    // PWM frequency formula:
-    // f = 16 MHz / (N * (1 + TOP))
-    // here N = 1
-    top = (16000000UL / frequency) - 1;
+    // f = 16MHz / (N * (1 + TOP))  where N = 1
+    unsigned long top = (16000000UL / frequency) - 1;
 
-    OCR4A = (unsigned int)top;  // sets frequency
+    OCR4A = (unsigned int)top;  // sets frequency via TOP
     OCR4C = OCR4A / 2;          // 50% duty cycle
 }
